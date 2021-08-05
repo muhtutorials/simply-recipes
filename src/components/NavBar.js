a0397527dfd9c2f1d0ac288cd5f24c50e2379e6b
Igor <igorwho@yandex.ru>
2021-08-05 22:09:42 +0300

    Two

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
