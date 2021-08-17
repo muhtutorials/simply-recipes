05cde28d01abf723b861737dfcdb814cf574d533
Igor <igorwho@yandex.ru>
2021-08-17 20:57:44 +0300

    Five

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 
 export default function About() {
       return (
         <Layout>
-            <h1>About</h1>
+            <main className="page">
+                <section className="about-page">
+                    <article>
+                        <h2>I'm baby coloring book poke taxidermy</h2>
+                        <p>Taxidermy forage glossier letterpress heirloom before they sold out you probably haven't heard of them banh mi biodiesel chia.</p>
+                        <p>Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia retro.</p>
+                        <Link to="/contact" className="btn">
+                            contact
+                        </Link>
+                    </article>
+                    <StaticImage
+                        src="../assets/images/about.jpeg"
+                        alt="person pouring salt"
+                        className="about-img"
+                        placeholder="blurred"
+                    />
+                </section>
+            </main>
         </Layout>
     );
 }
